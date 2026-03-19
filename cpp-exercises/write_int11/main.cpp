c5c2581eb62eef67f2250f9af4e04f1b22dcf6f0
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-03-19 09:30:29 +0100

    complete write11

--- cpp-exercises/write_int11/main.cpp
+++ cpp-exercises/write_int11/main.cpp
@@ -2,23 +2,50 @@
 #include <fstream>
 #include <vector>
 #include <iterator>
+#include <bitset>
 
 class BitWriter
 {
 public:
 	BitWriter(std::ostream& os);
 	~BitWriter();
+	template <typename T> void operator()(int n, const T& val);
+	void flushBuffer(uint8_t val = 0);
 
 private:
 	std::ostream& os_;
 	uint8_t n_;
 	uint8_t buffer_;
+	void writeBit(uint8_t currBit);
 };
 
 BitWriter::BitWriter(std::ostream& os) : os_(os), n_(0), buffer_(0) {}
 
 BitWriter::~BitWriter()
 {
+	flushBuffer();
+}
+
+void BitWriter::writeBit(uint8_t currBit) {
+	buffer_ = (buffer_ << 1) | currBit;
+	n_++;
+	if (n_ == 8) {
+		os_.put(buffer_);
+		n_ = 0;
+	}
+}
+
+template <typename T>
+void BitWriter::operator()(int n, const T& val) {
+	for (int i = (n - 1); i >= 0; i--) {
+		uint8_t currBit = (val >> i) & 1;
+		writeBit(currBit);
+	}
+}
+
+void BitWriter::flushBuffer(uint8_t bit) {
+	if (bit > 1) bit = 1;
+	while (n_ > 0) writeBit(bit);
 }
 
 int main(int argc, char** argv) {
@@ -49,7 +76,12 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	
+	std::vector<int32_t> v{ std::istream_iterator<int32_t>(is), {} };
+
+	BitWriter bw(os);
+	for (const auto& x : v) {
+		bw(11, x);
+	}
 
 	return EXIT_SUCCESS;
 }
