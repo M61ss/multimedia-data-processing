142db5034111bd365f7dd3d33e6c10e3c146d20c
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-04-09 19:36:13 +0200

    refactor

--- gradient/main.cpp
+++ gradient/main.cpp
@@ -4,25 +4,25 @@
 
 class Image {
 public:
-	Image(const size_t& dimX, const size_t& dimY) : dimX_(dimX), dimY_(dimY) {}
+	Image(const size_t& dimX, const size_t& dimY) : pixelX_(dimX), pixelY_(dimY) {}
 
 	std::vector<std::vector<uint8_t>> setRow(const size_t& index, const std::vector<uint8_t>& row) {
-		if (index >= dimY_) throw std::out_of_range("Index out of image Y range.");
-		if (row.size() >= dimX_) throw std::out_of_range("Index out of image X range.");
+		if (index >= pixelY_) throw std::out_of_range("Index out of Y pixel range.");
+		if (row.size() >= pixelX_) throw std::out_of_range("Index out of X pixel range.");
 		matrix_[index] = row;
 
 		return matrix_;
 	}
 
 	std::vector<uint8_t> getRow(const size_t& index) {
-		if (index >= dimY_) throw std::out_of_range("Index out of image Y range.");
+		if (index >= pixelY_) throw std::out_of_range("Index out of Y pixel range.");
 		
 		return matrix_[index];
 	}
 
 	std::vector<std::vector<uint8_t>> setColumn(const size_t& index, const std::vector<uint8_t>& col) {
-		if (index >= dimX_) throw std::out_of_range("Index out of image X range.");
-		if (col.size() >= dimY_) throw std::out_of_range("Index out of image Y range.");
+		if (index >= pixelX_) throw std::out_of_range("Index out of X pixel range.");
+		if (col.size() >= pixelY_) throw std::out_of_range("Index out of Y pixel range.");
 		for (uint8_t i = 0; i < col.size(); i++) {
 			matrix_[i][index] = col[i];
 		}
@@ -31,14 +31,14 @@ public:
 	}
 
 	std::vector<uint8_t> getColumn(const size_t& index) {
-		if (index >= dimX_) throw std::out_of_range("Index out of image X range.");
+		if (index >= pixelX_) throw std::out_of_range("Index out of X pixel range.");
 
 		return matrix_[index];
 	}
 private:
 	std::vector<std::vector<uint8_t>> matrix_;
-	size_t dimX_;
-	size_t dimY_;
+	size_t pixelX_;
+	size_t pixelY_;
 };
 
 int main(int argc, char** argv) {
