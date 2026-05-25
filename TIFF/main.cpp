978dae83753af8fd8ba2efd6a986c5c7314544c3
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-05-25 15:48:16 +0200

    read tiff header

---
+++ TIFF/main.cpp
@@ -0,0 +1,74 @@
+#include <vector>
+#include <fstream>
+
+class Image {
+private:
+	size_t width_;
+	size_t height_;
+	std::vector<uint8_t> data_;
+
+public:
+	Image() : width_(-1), height_(-1) {}
+
+	const uint8_t& operator()(const size_t& i, const size_t& j) const {
+		return data_[i * width_ + j];
+	}
+	uint8_t& operator()(const size_t& i, const size_t& j) {
+		return const_cast<uint8_t&>(static_cast<const Image*>(this)->operator()(i, j));
+	}
+
+	const size_t& width() const { return width_; }
+	const size_t& height() const { return height_; }
+	const std::vector<uint8_t>& data() const { return data_; }
+};
+
+Image readTIFF(std::istream& is) {
+	Image img;
+	std::string byteOrder(2, '\0');
+	is.read(byteOrder.data(), 2);
+	if (byteOrder != "II") {
+		return img;
+	}
+
+	size_t n = 0;
+	is.read(reinterpret_cast<char*>(&n), 2);
+	if (n != 42) {
+		return img;
+	}
+
+	size_t IFDoffset = 0;
+	is.read(reinterpret_cast<char*>(&IFDoffset), 4);
+	is.seekg(IFDoffset);
+
+	return img;
+}
+
+void savePAM(std::ostream& os, const Image& img) {
+	os << "P7"
+		<< "WIDTH " << img.width() << "\n"
+		<< "HEIGHT " << img.height() << "\n"
+		<< "DEPTH 1\n"
+		<< "MAXVAL 255\n"
+		<< "TUPLTYPE GRAYSCALE\n"
+		<< "ENDHDR\n";
+	os.write(reinterpret_cast<const char*>(img.data().data()), sizeof(uint8_t) * img.width() * img.height());
+}
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		return 1;
+	}
+
+	std::ifstream is(argv[1], std::ios::binary);
+	if (!is) {
+		return 1;
+	}
+	std::ofstream os(argv[2], std::ios::binary);
+	if (!os) {
+		return 1;
+	}
+
+	Image img = readTIFF(is);
+
+	return 0;
+}
