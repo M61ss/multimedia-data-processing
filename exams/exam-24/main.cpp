fe3447e29d8876ef70a3d0509bb4c23a749992fe
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-05-27 18:13:58 +0200

    correct endianess

--- exams/exam-24/main.cpp
+++ exams/exam-24/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <bit>
 
 struct PDBheader {
 	std::string name_;
@@ -14,7 +15,7 @@ struct PDBheader {
 	uint32_t appInfoID_ = 0;
 	uint32_t sortInfoID_ = 0;
 	std::string type_;
-	std::string creator_ = 0;
+	std::string creator_;
 	uint32_t uniqueIDseed_ = 0;
 	uint32_t nextRecordListID_ = 0;
 	uint16_t numberOfRecords_ = 0;
@@ -43,17 +44,27 @@ public:
 		is_.read(reinterpret_cast<char*>(&hdr_.attr1_), 1);
 		is_.read(reinterpret_cast<char*>(&hdr_.attr2_), 1);
 		is_.read(reinterpret_cast<char*>(&hdr_.version_), 2);
+		hdr_.version_ = std::byteswap(hdr_.version_);
 		is_.read(reinterpret_cast<char*>(&hdr_.creationDate_), 4);
+		hdr_.creationDate_ = std::byteswap(hdr_.creationDate_);
 		is_.read(reinterpret_cast<char*>(&hdr_.modificationDate_), 4);
+		hdr_.modificationDate_ = std::byteswap(hdr_.modificationDate_);
 		is_.read(reinterpret_cast<char*>(&hdr_.lastBackupDate_), 4);
+		hdr_.lastBackupDate_ = std::byteswap(hdr_.lastBackupDate_);
 		is_.read(reinterpret_cast<char*>(&hdr_.modificationNumber_), 4);
+		hdr_.modificationNumber_ = std::byteswap(hdr_.modificationNumber_);
 		is_.read(reinterpret_cast<char*>(&hdr_.appInfoID_), 4);
+		hdr_.appInfoID_ = std::byteswap(hdr_.appInfoID_);
 		is_.read(reinterpret_cast<char*>(&hdr_.sortInfoID_), 4);
+		hdr_.sortInfoID_ = std::byteswap(hdr_.sortInfoID_);
 		is_.read(hdr_.type_.data(), 4);
 		is_.read(hdr_.creator_.data(), 4);
 		is_.read(reinterpret_cast<char*>(&hdr_.uniqueIDseed_), 4);
+		hdr_.uniqueIDseed_ = std::byteswap(hdr_.uniqueIDseed_);
 		is_.read(reinterpret_cast<char*>(&hdr_.nextRecordListID_), 4);
+		hdr_.nextRecordListID_ = std::byteswap(hdr_.nextRecordListID_);
 		is_.read(reinterpret_cast<char*>(&hdr_.numberOfRecords_), 2);
+		hdr_.numberOfRecords_ = std::byteswap(hdr_.numberOfRecords_);
 	}
 
 	void printHeader() {
@@ -70,8 +81,11 @@ public:
 		for (uint16_t i = 0; i < hdr_.numberOfRecords_; i++) {
 			RecordInfoEntry rie;
 			is_.read(reinterpret_cast<char*>(&rie.recordDataOffset_), 4);
+			rie.recordDataOffset_ = std::byteswap(rie.recordDataOffset_);
 			is_.read(reinterpret_cast<char*>(&rie.recordAttributes_), 1);
 			is_.read(reinterpret_cast<char*>(&rie.uniqueID_), 3);
+			rie.uniqueID_ = std::byteswap(rie.uniqueID_);
+			rie.uniqueID_ >>= 8;
 			ries_.push_back(rie);
 		}
 	}
