e9d86efd6c691e8baf05cd42e9f56ec424297382
Mattia Massarenti <121773111+M61ss@users.noreply.github.com>
2026-06-13 17:52:19 +0200

    simple entropy tool

---
+++ entropy/main.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <iostream>
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		return 1;
+	}
+
+	std::ifstream is(argv[1], std::ios::binary);
+	if (!is) {
+		return 1;
+	}
+
+	is.seekg(0, std::ios::end);
+	size_t nBytes = is.tellg();
+	is.seekg(0, std::ios::beg);
+
+	std::vector<uint8_t> data{ std::istream_iterator<uint8_t>(is), {} };
+
+	std::map<uint8_t, double> probs;
+	for (const auto& x : data) {
+		++probs[x];
+	}
+	for (auto& [val, prob] : probs) {
+		prob /= nBytes;
+	}
+	
+	double entropy = 0;
+	for (const auto& [val, prob] : probs) {
+		entropy += prob * log2(1 / prob);
+	}
+
+	std::cout << "File '" << argv[1] << "':\n\n"
+		<< " - Entropy: " << entropy << "\n";
+
+	return 0;
+}
